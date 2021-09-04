2f0a435acb13831d8b22b0a5647013bded181fec
Camilo Mazo <camilo.mazo2@udea.edu.co>
2021-09-04 02:48:53 -0500

    Version final 1.1

--- PRACTICA4/main.cpp
+++ PRACTICA4/main.cpp
@@ -124,47 +124,5 @@ int main(){
         }
 
     }
-
-    /*
-    enrutadores.modificacion1("B",4);
-    enrutadores.modificacion1("C",2);
-    enrutadores.modificacion2("A");
-
-    enrutadores.modificacion1("A",4);
-    enrutadores.modificacion1("C",1);
-    enrutadores.modificacion1("D",5);
-    enrutadores.modificacion2("B");
-
-    enrutadores.modificacion1("A",2);
-    enrutadores.modificacion1("B",1);
-    enrutadores.modificacion1("D",8);
-    enrutadores.modificacion1("E",10);
-    enrutadores.modificacion2("C");
-
-    enrutadores.modificacion1("B",5);
-    enrutadores.modificacion1("C",8);
-    enrutadores.modificacion1("E",2);
-    enrutadores.modificacion1("F",6);
-    enrutadores.modificacion2("D");
-
-    enrutadores.modificacion1("C",10);
-    enrutadores.modificacion1("D",2);
-    enrutadores.modificacion1("F",2);
-    enrutadores.modificacion2("E");
-
-    enrutadores.modificacion1("D",6);
-    enrutadores.modificacion1("E",2);
-    enrutadores.modificacion2("F");
-
-    enrutadores.calcularRuta("F","B");
-
-
-    enrutadores.agregarEnrutador("D","A",2);
-    enrutadores.modificacion2("D");
-
-    enrutadores.eliminarEnrutador("B");
-
-    enrutadores.modificarCosto("D","A",4);
-    enrutadores.modificacion2("D");*/
     return 0;
 }
